fc7e422be71750646041df6c102a7fe207f72850
konradit <konradit@users.noreply.github.com>
2018-06-03 09:49:19 +0200

    Add python script

---
+++ yi-fw-dl.py
@@ -0,0 +1,62 @@
+import json
+import urllib.request
+import urllib.parse
+import argparse
+import textwrap
+import requests
+import sys
+ap = argparse.ArgumentParser()
+ap.add_argument("camera",action="store",type=str)
+ap.add_argument("-a", action="store_true", default=False)
+args = vars(ap.parse_args())
+
+def print_data(choice):
+	print("SN:",j[choice]["snPrefix"],"- HW Code:",j[choice]["hardwareCode"])
+	print("")
+	print("Version",j[choice]["firmwareCode"],"- MD5",j[choice]["md5Code"])
+	print("")
+	print("Release notes:")
+	print(textwrap.dedent(j[choice]["firmwareMemo"]).strip())
+	print("")
+	print("Press enter to download", end="")
+	input()
+	fw_filename="firmware-"+j[choice]["firmwareCode"].replace(".","_")+".zip"
+	download(j[choice]["firmwareUrl"], fw_filename)	
+
+def download(url, fw_filename):
+	with open(fw_filename, "wb") as f:
+		print("Downloading %s" % fw_filename)
+		response = requests.get(url, stream=True)
+		total_length = response.headers.get('content-length')
+
+		if total_length is None: # no content length header
+			f.write(response.content)
+		else:
+			dl = 0
+			total_length = int(total_length)
+			for data in response.iter_content(chunk_size=4096):
+				dl += len(data)
+				f.write(data)
+				done = int(50 * dl / total_length)
+				sys.stdout.write("\r[%s%s]" % ('=' * done, ' ' * (50-done)) )    
+				sys.stdout.flush()
+camera_url=""
+if args["camera"].startswith("J"):
+	camera_url="https://www.yitechnology.com/support/actionlite_input"
+
+elif args["camera"].startswith("Z"):
+	camera_url="https://www.yitechnology.com/support/action4k_input"
+data = urllib.parse.urlencode({'id' : '3','sn'  : args["camera"]})
+data = data.encode('utf-8')
+raw = urllib.request.urlopen(camera_url, data=data).read()
+if args["a"] == False:
+	j=json.loads(raw)
+	print_data(0)
+else:
+	j=json.loads(raw)
+	for index, i in enumerate(j):
+		print(index,"-",i["firmwareCode"])
+	choice=int(input("Firmware download: [0 - " + str(len(j)-1) + "]: "))
+	print_data(choice)
+
+
